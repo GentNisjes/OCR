a7ff7999ddd2a6c29237066262c409814fa4886f
Stijn <stijngeens07@gmail.com>
2024-07-08 17:42:53 +0200

    further finetuning for ocr

--- Ocr.py
+++ Ocr.py
@@ -5,10 +5,15 @@ def extract_text_from_image(image_path):
     # Open an image file
     with Image.open(image_path) as img:
         # Use pytesseract to do OCR on the image
-        text = pytesseract.image_to_string(img)
+        # Adding custom config
+        # nld+eng: for the languages to detect
+        # --psm 6: Assume a single uniform block of text.
+        custom_config = r'-l nld+eng --psm 6'
+        text = pytesseract.image_to_string(img, config=custom_config)
     return text
 
 # Example usage
-image_path = "./images/Ticket1.jpg"
+# Preprocessed image as image path
+image_path = "./images/no_border_Ticket3.jpg"
 extracted_text = extract_text_from_image(image_path)
 print(extracted_text)
